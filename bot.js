c255a2d3eb19eed41b7b650d4d4877bf53bbdbb1
ahmeedlivee <53989172+ahmeedlivee@users.noreply.github.com>
2019-08-23 02:14:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,14 +29,46 @@ client.on('message', message => {
             })
   	}
 
-  	if (message.content === '!help') {
-		  message.delete(message)
-            message.author.send("**Commands** \n !help \n !fortnite")
-         message.reply(":white_check_mark: Please Check Your **DM**'s!").then(m => {
+  if (message.content === '!test') {
+		    message.delete(message)
+		message.author.send("**Fortnite (New Accs Every 6-9 Hours)** \n http://bin.shortbin.eu:8080/AboZTQwfXs")
+			message.reply(":white_check_mark: Please Check Your **DM**'s!").then(m => {
                 setTimeout(() => {
                     m.delete(m)
                 }, 5000); //5 seconds
-  		
+            })
+  	}
+
+  	if (message.content === '!help') {
+		message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
             })
   	}
   
