9deefda1cec1893b7526b4fadb8c4c5989e0ac77
ahmeedlivee <53989172+ahmeedlivee@users.noreply.github.com>
2019-08-23 02:29:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,11 +21,7 @@ client.on('message', message => {
 	
     if (message.content === '!fortnite') {
 		    message.delete(message)
-		   let hex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		      let embed = new Discord.RichEmbed()
-            .addField("Updates Every 6-9 Hours", 'http://bin.shortbin.eu:8080/AboZTQwfXs')
-            .setColor(hex)
-            message.author.send(embed)
+		 
 			message.reply(":white_check_mark: Please Check Your **DM**'s!").then(m => {
                 setTimeout(() => {
                     m.delete(m)
@@ -35,11 +31,7 @@ client.on('message', message => {
 
   	if (message.content === '!help') {
 		  message.delete(message)
-		   let hex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		      let embed = new Discord.RichEmbed()
-            .addField("Commands", '!help - !fortnite')
-            .setColor(hex)
-            message.author.send(embed)
+		 
   		message.reply(":white_check_mark: Please Check Your **DM**'s!").then(m => {
                 setTimeout(() => {
                     m.delete(m)
